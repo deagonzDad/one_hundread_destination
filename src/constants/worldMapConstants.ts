294279ff7e91808093e56a198cdf6058e4533d06
carael <deagonz.5@gmail.com>
2025-08-01 02:03:40 -0600

    refactor: Remove commented-out image mapping code

--- src/constants/worldMapConstants.ts
+++ src/constants/worldMapConstants.ts
@@ -1,6 +1,3 @@
-// import type { ImageMetadata } from "astro";
-// import { getImage } from "astro:assets";
-// import type { Dictionary } from "src/types/continents";
 export const WIDTH_BTN_CTN: number = 100;
 export const HEIGHT_BTN_CTN: number = 70;
 export const QUERYSTRING_COUNTRY_NAME: string = "country";
@@ -39,18 +36,3 @@ export const POSITION_NAME_MAP = {
     continentImg: "Australia",
   },
 };
-
-//mapper imgs
-// const images = import.meta.glob<{ default: ImageMetadata }>(
-//   "/src/assets/images/continents/*.{jpeg,jpg,png,gif}",
-// );
-// export const imageMapping: Dictionary<string, string> = {};
-// const mappedData = Object.entries(POSITION_NAME_MAP).map(
-//   async ([, { continentImg }]) => {
-//     const imgPath = `/src/assets/images/continents/${continentImg}.png`;
-//     if (!images[imgPath]) throw new Error(`${imgPath} does not exist :(`);
-//     const tempImg = await getImage({ src: images[imgPath]() });
-//     imageMapping[continentImg] = tempImg.src;
-//   },
-// );
-// await Promise.all(mappedData);
