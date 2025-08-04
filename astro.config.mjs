b1db880487b8233410848bc063c902d965e89aa3
carael <deagonz.5@gmail.com>
2025-08-03 22:59:31 -0600

    feat(seo): add sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from "astro/config";
 
+import sitemap from "@astrojs/sitemap";
+
 export default defineConfig({
   trailingSlash: "always",
   output: "static",
   site: "https://one-hundread-destination.pages.dev",
+  integrations: [sitemap()],
 });
